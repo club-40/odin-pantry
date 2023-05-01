7a9d9f43baa0f90dbcd6ad52d2406aa209abb97f
root <prantikdas2004@gmail.com>
2023-05-02 01:01:23 +0530

    [feat] adapt root router to react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,20 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import routes from "@/misc/routing";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root: HTMLElement = document.getElementById("root")!;
 
+const queryClient = new QueryClient();
+
 createRoot(root).render(
   <React.StrictMode>
-    <RouterProvider router={createBrowserRouter(routes)} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={createBrowserRouter(routes(queryClient))} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
