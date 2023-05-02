4f4687e857d0d0f48b63e331b77e4d33dbf6a612
root <prantikdas2004@gmail.com>
2023-05-02 14:52:53 +0530

    [fix] eslint errors

--- frontend/src/services/store-slices/bear.ts
+++ frontend/src/services/store-slices/bear.ts
@@ -8,8 +8,14 @@ export interface BearSlice {
   addBear: () => void;
 }
 
+const INITIAL_BEAR_COUNT = 0;
+const BEAR_INCREMENT = 1;
+
 export const createBearSlice: CreateSlice<BearSlice> = (set) => ({
-  bears: 0,
+  bears: INITIAL_BEAR_COUNT,
   addBear: () =>
-    set((state: StateUnion) => ({ ...state, bears: state.bears + 1 })),
+    set((state: StateUnion) => ({
+      ...state,
+      bears: state.bears + BEAR_INCREMENT,
+    })),
 });
